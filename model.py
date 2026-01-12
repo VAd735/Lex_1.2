20775bb9065555f990a80e8d658d312584ba2d70
Bartashchuk Vadym <124402@student.san.edu.pl>
2026-01-13 00:31:54 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 from llama_cpp import Llama
 import os
 
-MODEL_PATH = "models/qwen2.5-3b-instruct-q4_k_m.gguf"
+MODEL_PATH = "models/qwen3b.gguf"
 
 if not os.path.exists(MODEL_PATH):
-    raise RuntimeError("Model file not found. Run download_model.py first.")
+    raise RuntimeError("Model file not found. Запусти download_model.py")
 
 llm = Llama(
     model_path=MODEL_PATH,
