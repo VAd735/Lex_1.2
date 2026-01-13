5a0fc5d0098e5ee1cb5ad2d18ac58b2498f45166
Bartashchuk Vadym <124402@student.san.edu.pl>
2026-01-13 09:50:20 +0100

    Update model path and retain Llama initialization

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from llama_cpp import Llama
 import os
 
-MODEL_PATH = "models/qwen3b.gguf"
+MODEL_PATH = "models/qwen2.5-3b-instruct-q4_k_m.gguf"
 
 if not os.path.exists(MODEL_PATH):
     raise RuntimeError("Model file not found. Запусти download_model.py")
@@ -15,3 +15,4 @@ llm = Llama(
 def generate_answer(text):
     result = llm(f"[INST] {text} [/INST]", max_tokens=150)
     return result["choices"][0]["text"].strip()
+
